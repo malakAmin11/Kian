77dad2670066c8e3531ff8e144c95ccf34acc3c7
Malak <malak.amin820@gmail.com>
2026-08-30 18:48:50 +0300

    Add files via upload

--- School_Management/main.cpp
+++ School_Management/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <Person.h>
+#include <Student.h>
+#include <Teacher.h>
+#include <Staff.h>
+#include <Course.h>
+#include <Classroom.h>
 
+using namespace std;
+
+int main()
+{
+    Course s;
+    s.informations();
+    s.print();
+
+    return 0;
+}
