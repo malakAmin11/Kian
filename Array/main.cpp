abbfeeb7367611b529e7d7eb557c9d2333827fc2
Malak <malak.amin820@gmail.com>
2026-09-01 02:38:05 +0300

    Add files via upload

--- Array/main.cpp
+++ Array/main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <arrayManager.h>
 
+//session 1---- date structures
+
+using namespace std;
+
+int main()
+{
+    arrayManager a;
+    a.insertLast(1);
+    a.insertLast(2);
+    a.insertLast(3);
+    a.insertLast(4);
+
+    a.insertAtPosition(10, 1);
+
+    a.removeLast();
+
+    a.removeAtPosition(1);
+    cout<<a.Search(3) <<endl;
+    a.edit(1, 20);
+    a.print();
+
+
+
+    return 0;
+}
